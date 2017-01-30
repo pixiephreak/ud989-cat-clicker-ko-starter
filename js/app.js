71e863fce8fe982599892de88a01aa2f877bed37
Jane Claire <pixiephreak@Janes-MBP.home>
2017-01-30 12:51:32 -0500

    update using with

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
-var ViewModel = function(){
+var Cat = function(){
 	this.clickCounter = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/1413379559_412a540d29_z.jpg');
 	this.imgAttribution = ko.observable('something');
 	this.level = ko.observable('');
-
-	this.incrementCounter = function(){
-		this.clickCounter(this.clickCounter() + 1);
-	}
+	this.nicknames = ko.observableArray([
+		{nickname: 'tabs'}, {nickname: 'tabsee'},{nickname: 'tabster'}
+		]);
 
 	this.level = ko.computed(function(){
 		if(this.clickCounter() <= 5){
@@ -20,4 +19,22 @@ var ViewModel = function(){
 	}, this);
 }
 
+
+var ViewModel = function(){
+
+	this.currentCat = ko.observable(new Cat());
+
+	this.incrementCounter = function(){
+		this.clickCounter(this.currentCat().clickCounter() + 1);
+	}
+
+
+
+
+
+
+}
+
 ko.applyBindings(new ViewModel());
+
+
